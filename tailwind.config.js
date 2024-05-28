3034030ec5f912bf1473493fa830f1019c1c2724
PABLO ROS PROFE <pros7@xtec.cat>
2024-05-28 19:55:07 +0200

    funcionant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js}',
     './public/**/*.{html,js}',
-    './dist/*.{html,js}',
+    './src/**/*.{html,js}',
     './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require('flowbite/plugin')],
 };
